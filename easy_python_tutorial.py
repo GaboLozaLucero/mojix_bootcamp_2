8690037b9f80205c8bf3df827f07835603d4b770
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-11 20:42:29 -0400

    Update easy_python_tutorial.py

--- easy_python_tutorial.py
+++ easy_python_tutorial.py
@@ -6,7 +6,8 @@ st.title('Easy tutorial for PYTHON BEGINNERS')
 st.write('Simple but effective tips for every python lovers')
 
 st.subheader('1. Walrus operator')
-st.text('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
+st.text('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that')
+st.text('lets you assign value to a variable within an expression like conditional statements, loops, etc.')
 
 col1, col2, col3 = st.columns(3)
 
