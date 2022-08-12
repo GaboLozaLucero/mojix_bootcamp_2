7239e8e537e7cc386966d6dd124874c00ec34063
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-11 21:01:11 -0400

    Update easy_python_tutorial.py

--- easy_python_tutorial.py
+++ easy_python_tutorial.py
@@ -6,7 +6,7 @@ st.title('Easy tutorial for PYTHON BEGINNERS')
 st.write('Simple but effective tips for every python lovers')
 
 st.header('1. Walrus operator')
-st.text('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value \nto a variable within an expression like conditional statements, loops, etc.')
+st.text_area('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
 
 st.subheader('Example')
 walrus_operator = '''Mylist = [1,2,3]
@@ -19,7 +19,7 @@ outputWO = '''3'''
 st.code(outputWO, language='python')
 
 st.header('2. Splitting a string')
-st.text('If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the \nstring operations a lot easier!')
+st.text_area('If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!')
 
 st.subheader('Example')
 string_split = '''string = "hello world"
@@ -29,3 +29,16 @@ st.code(string_split, language='python')
 st.subheader('Output')
 outputSS = '''['hello', 'world']'''
 st.code(outputSS, language='python')
+
+st.header('3. Reversing a string')
+st.text_area('If you want to reverse a given string, you can do that with only one line of code using the negative indexing of the string.')
+
+st.subheader('Example')
+reversing = '''string = "hello world"
+a=string[::-1]
+print(a)'''
+st.code(reversing, language='python')
+
+st.subheader('Output')
+outputR = '''['hello', 'world']'''
+st.code(outputR, language='python')
