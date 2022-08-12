ba91bbdbcf586702443f5c2db6c7518cbed7ae59
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-11 21:06:40 -0400

    Update easy_python_tutorial.py

--- easy_python_tutorial.py
+++ easy_python_tutorial.py
@@ -6,7 +6,7 @@ st.title('Easy tutorial for PYTHON BEGINNERS')
 st.write('Simple but effective tips for every python lovers')
 
 st.header('1. Walrus operator')
-st.text_area('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
+st.write('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
 
 st.subheader('Example')
 walrus_operator = '''Mylist = [1,2,3]
@@ -31,7 +31,7 @@ outputSS = '''['hello', 'world']'''
 st.code(outputSS, language='python')
 
 st.header('3. Reversing a string')
-st.text_area('If you want to reverse a given string, you can do that with only one line of code using the negative indexing of the string.')
+st.write('If you want to reverse a given string, you can do that with only one line of code using the negative indexing of the string.')
 
 st.subheader('Example')
 reversing = '''string = "hello world"
