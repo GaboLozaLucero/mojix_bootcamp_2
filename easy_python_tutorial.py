654fef50fc6025de5049011502787625d5e729e7
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-11 20:34:58 -0400

    Update easy_python_tutorial.py

--- easy_python_tutorial.py
+++ easy_python_tutorial.py
@@ -3,8 +3,16 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title('Easy tutorial for PYTHON BEGINNERS')
 
-col1 = st.columns(1)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-  st.header("Walrus operator")
-  st.write(mylist = [1,2,3])
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
