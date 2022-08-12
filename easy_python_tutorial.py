44688024da96b03a72b313604200371e1b8e9c73
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-11 20:34:17 -0400

    Update easy_python_tutorial.py

--- easy_python_tutorial.py
+++ easy_python_tutorial.py
@@ -3,6 +3,8 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title('Easy tutorial for PYTHON BEGINNERS')
 
+col1 = st.columns(1)
+
 with col1:
   st.header("Walrus operator")
   st.write(mylist = [1,2,3])
