c617962079b2a04222358893173648b7d247535f
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-20 23:44:43 -0400

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -82,10 +82,10 @@ st.code(positives, language='python')
 st.dataframe(df_discrepancy)
 
 st.write('Now we group by the information obtained')
-df_discrepancy.groupby("Retail_Product_Level1Name").sum()
+df_product_lelve1_name = df_discrepancy.groupby("Retail_Product_Level1Name").sum()
 new_info = '''df_discrepancy.groupby("Retail_Product_Level1Name").sum()'''
 st.code(new_info, language='python')
-st.dataframe(df_discrepancy.describe)
+st.dataframe(df_product_lelve1_name)
 
 
 
