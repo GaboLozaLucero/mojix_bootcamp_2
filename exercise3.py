fa36d8c913543cc789ac964db5ac57826e51a27e
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-21 00:51:42 -0400

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -80,6 +80,7 @@ df_unders = df_discrepancy.groupby(["Diff", "Retail_Product_Name"]).sum()
 st.dataframe(df_unders)
 
 need = df_unders.head()
-st.bar_chart(pd.DataFrame(need))
+st.dataframe(need)
+#st.bar_chart(pd.DataFrame(need))
 
 
