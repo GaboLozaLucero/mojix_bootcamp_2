688d3b2c901f80c68312ec9666bbb1f309b12fa7
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-21 00:45:02 -0400

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -77,6 +77,6 @@ st.write('These products need RFID')
 #df_size = df_discrepancy.groupby("Retail_Product_Size").sum()
 #st.bar_chart(df_size.head())
 df_unders = df_discrepancy.groupby(["Diff", "Retail_Product_Name"]).sum()
-st.dataframe(df_unders, width=550);
+st.dataframe(df_unders);
 
 
