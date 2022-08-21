1b3f2670a64a41a3827e87918d75abebaf213c76
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2022-08-20 22:56:31 -0400

    adding dataframe df_a

--- exercise3.py
+++ exercise3.py
@@ -22,11 +22,38 @@ st.write('df_counted')
 df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
 st.dataframe(df_counted)
 
-st.subheader('Remove duplicated data')
+st.subheader('Remove duplicated data and rename to show real quantity')
 df_counted = df_counted.drop_duplicates("RFID")
 df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
 duplicates = '''df_counted = df_counted.drop_duplicates("RFID")
-df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
-df_B.sample(5)'''
+df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})'''
 st.code(duplicates, language='python')
 st.dataframe(df_B)
+
+st.write('select and show rows by retail info')
+rows_selected = ["Retail_Product_Color",
+"Retail_Product_Level1",
+"Retail_Product_Level1Name",
+"Retail_Product_Level2Name",
+"Retail_Product_Level3Name",
+"Retail_Product_Level4Name",
+"Retail_Product_Name",
+"Retail_Product_SKU",
+"Retail_Product_Size",
+"Retail_Product_Style",
+"Retail_SOHQTY"]
+df_A = df_expected[rows_selected]
+retail_info = '''rows_selected = ["Retail_Product_Color",
+"Retail_Product_Level1",
+"Retail_Product_Level1Name",
+"Retail_Product_Level2Name",
+"Retail_Product_Level3Name",
+"Retail_Product_Level4Name",
+"Retail_Product_Name",
+"Retail_Product_SKU",
+"Retail_Product_Size",
+"Retail_Product_Style",
+"Retail_SOHQTY"]
+df_A = df_expected[rows_selected]'''
+st.code(duplicates, language='python')
+st.dataframe(df_A)
